fa986816de61edf6717bb34772a777940505a3a5
Martin Schrodt <martin@schrodt.org>
2021-06-01 23:36:43 +0200

    add linux main function

---
+++ ProjectGenesis/mainLinux.cpp
@@ -0,0 +1,7 @@
+#include "intro.h"
+
+INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow );
+
+int main(int argc, char *argv[]) {
+    WinMain(NULL, NULL, NULL, 0);
+}
